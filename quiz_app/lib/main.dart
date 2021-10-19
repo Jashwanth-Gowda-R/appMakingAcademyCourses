f3e78eca64f54d48800914b02d9dfe588f0231a0
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-10-19 21:35:17 +0530

    added bassic structure and questions

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,115 +1,86 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const QuizApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+class QuizApp extends StatefulWidget {
+  const QuizApp({Key key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  _QuizAppState createState() => _QuizAppState();
+}
+
+class _QuizAppState extends State<QuizApp> {
+  List questions = [
+    {
+      "question": "How many World cup india as won in t20?",
+      "options_a": "4",
+      "options_b": "3",
+      "options_c": "1",
+      "options_d": "5",
+      "answer": "1"
+    },
+    {
+      "question": "How many colors in Google Logo?",
+      "options_a": "4",
+      "options_b": "3",
+      "options_c": "5",
+      "options_d": "6",
+      "answer": "4"
+    },
+    {
+      "question": "How many cups did CSK won in ipl?",
+      "options_a": "4",
+      "options_b": "3",
+      "options_c": "5",
+      "options_d": "6",
+      "answer": "4"
+    },
+    {
+      "question": "which is the team to win the wcup in year 1983",
+      "options_a": "Sri Lanka",
+      "options_b": "Pakistan",
+      "options_c": "India",
+      "options_d": "australia",
+      "answer": "India"
+    },
+  ];
+  var rightAnswerCount = 0;
+  var wrongAnswerCount = 0;
+  var currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("${questions[currentIndex]["question"]}"),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("${questions[currentIndex]["options_a"]}"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("${questions[currentIndex]["options_b"]}"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("${questions[currentIndex]["options_c"]}"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("${questions[currentIndex]["options_d"]}"),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
