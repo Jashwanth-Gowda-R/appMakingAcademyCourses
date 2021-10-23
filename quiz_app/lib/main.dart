d54956c363e86d2026e95c140a912914436756ff
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-10-24 00:06:48 +0530

    add style to app

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -91,48 +91,104 @@ class _QuizAppState extends State<QuizApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+          backgroundColor: Colors.blueGrey[900],
           body: Container(
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("${questions[currentIndex]["question"]}"),
-            ElevatedButton(
-              onPressed: () {
-                validateAnswer(questions[currentIndex]["options_a"]);
-              },
-              child: Text("${questions[currentIndex]["options_a"]}"),
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    color: Colors.white,
+                    margin: const EdgeInsets.all(8),
+                    padding: const EdgeInsets.all(8),
+                    child: Text(
+                      "${questions[currentIndex]["question"]}",
+                      style: const TextStyle(fontSize: 24),
+                      textAlign: TextAlign.center,
+                    )),
+                Container(
+                  width: double.infinity,
+                  height: 40,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blueGrey,
+                      elevation: 0,
+                    ),
+                    onPressed: () {
+                      validateAnswer(questions[currentIndex]["options_a"]);
+                    },
+                    child: Text("${questions[currentIndex]["options_a"]}"),
+                  ),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 40,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blueGrey,
+                      elevation: 0,
+                    ),
+                    onPressed: () {
+                      validateAnswer(questions[currentIndex]["options_b"]);
+                    },
+                    child: Text("${questions[currentIndex]["options_b"]}"),
+                  ),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 40,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blueGrey,
+                      elevation: 0,
+                    ),
+                    onPressed: () {
+                      validateAnswer(questions[currentIndex]["options_c"]);
+                    },
+                    child: Text("${questions[currentIndex]["options_c"]}"),
+                  ),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 40,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blueGrey,
+                      elevation: 0,
+                    ),
+                    onPressed: () {
+                      validateAnswer(questions[currentIndex]["options_d"]);
+                    },
+                    child: Text("${questions[currentIndex]["options_d"]}"),
+                  ),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Text(
+                  "No of questions : ${questions.length}   Right Answer :$rightAnswerCount    Wrong Answer:$wrongAnswerCount",
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                TextButton(
+                    onPressed: () {
+                      reset();
+                    },
+                    child: const Text("Reset and try again"))
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                validateAnswer(questions[currentIndex]["options_b"]);
-              },
-              child: Text("${questions[currentIndex]["options_b"]}"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                validateAnswer(questions[currentIndex]["options_c"]);
-              },
-              child: Text("${questions[currentIndex]["options_c"]}"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                validateAnswer(questions[currentIndex]["options_d"]);
-              },
-              child: Text("${questions[currentIndex]["options_d"]}"),
-            ),
-            Text(
-              "No of questions : ${questions.length}   Right Answer :$rightAnswerCount    Wrong Answer:$wrongAnswerCount",
-            ),
-            TextButton(
-                onPressed: () {
-                  reset();
-                },
-                child: const Text("Reset and try again"))
-          ],
-        ),
-      )),
+          )),
     );
   }
 }
